500a7e70129aacffe1bf2f47de7c89bf3fce15e3
Sam Vilain <sam@vilain.net>
2014-07-06 21:22:54 -0700

    Try in vain to get ParseQueryAdapter to do anything at all.
    Perhaps this isn't even necessary to get working, because this way of
    hooking it up will need to be replaced by a version which uses a custom
    view anyway.  It would just be nice to get a quick view of the data!

--- src/org/ashanet/ListProjectsActivity.java
+++ src/org/ashanet/ListProjectsActivity.java
@@ -2,14 +2,32 @@ package org.ashanet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import com.parse.ParseObject;
+import com.parse.ParseQueryAdapter;
 
 public class ListProjectsActivity extends Activity
 {
+    private ParseQueryAdapter<ParseObject> adapter;
+    private ListView lvProjects;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_list_projects);
+        Log.d("DEBUG", "Creating Parse Query Adapter");
+        adapter = new ParseQueryAdapter<ParseObject>
+            (this, "Project"); //, android.R.layout.simple_list_item_1);
+        Log.d("DEBUG", "Setting text key");
+        adapter.setTextKey("name");
+        adapter.setImageKey("projectImage");
+ 
+        lvProjects = (ListView) findViewById(R.id.lvProjects);
+        Log.d("DEBUG", "Connecting adapter");
+        lvProjects.setAdapter(adapter);
+        Log.d("DEBUG", "done");
     }
 }
